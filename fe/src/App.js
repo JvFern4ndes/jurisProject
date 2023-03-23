1e65eb7f1dd450c3a01bb1b3dbb76eb87c421fcc
João Victor <jvfernandes.contato@gmail.com>
2023-03-22 23:58:47 -0300

    feat: transforming the typescript code in javascript

---
+++ fe/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Olá mundo</h1>
+    </>
+  );
+}
